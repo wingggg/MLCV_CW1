c3ab30478f088fda1fa6a1e0086baa23573c7711
Christoforos Santorinaios <Christoforos Santorinaios>
2016-02-14 17:20:46 +0000

    improvements to showFace function

--- showFace.m
+++ showFace.m
@@ -1,12 +1,18 @@
 function [ output_args ] = showFace( face )
 %SHOWFACE shows the image corresponding to a face vector
-%   Wrapper function for ease of use when printing out faces
+%   Wrapper function for ease of use when printing out faces. Input is a
+%   vector 2576x1 or 1x2576
 
-if size(face,1)==2756
+
+if size(face,1)~=2576 && size(face,2)~=2576
+    disp('wrong face vector size');
+    return;
+elseif size(face,1)==2576
     face=reshape(face,56,46);
-else
+    imshow(mat2gray(face));
+elseif size(face,2)==2576
     face=reshape(face',56,46);
-imshow(mat2gray(face));
-
+    imshow(mat2gray(face));
+   
 end
 
