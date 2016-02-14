6ada74e787fef8a6ccf9ffdfe4874f9779f0026c
Christoforos Santorinaios <Christoforos Santorinaios>
2016-02-14 16:34:32 +0000

    Working code for full procedure for Q1.

---
+++ Q1.m
@@ -0,0 +1,36 @@
+%Script for Q1
+% 
+load('face.mat')
+validationPC=0.1; %percentage of data to be used as validation data
+M=50; %the largest M eigenvalues will be picked. 
+
+[Train, Test] = crossvalind('HoldOut', size(X,2), validationPC); %produce crossvalidation indices
+trainingSet=X(:,Train); %create training and test sets according to indices
+testSet=X(:,Test);
+N=size(trainingSet,2);
+
+avgFace=mean(trainingSet,2);  %Average face
+A=trainingSet-repmat(avgFace,1,N); %matrix of phi values
+
+S=(1/N)*A*A'; %As asked in Q2 of spec
+[V,D]=eig(S);  %V are the RIGHT eigenvectors, D are the corresponding eigenvalues
+eigenvalues=diag(D);
+
+%BLOCK BELOW MIGHT BE WRONG 
+% for index=1:M      % Kinda hacky way to find the indices of the top M eigenvalues
+%     [eigenvalue,I]=max(eigenvalues);
+%     maxInd(index)=I;     %maxInd are the indices of the top M eigenvalues
+%     eigenvalues(I)=-inf; 
+% end
+
+%we now use the indices to extract the eigenvectors
+
+bestEigenvectors=V(:,1:M);
+
+
+for i=1:M
+    subplot(floor(M/2),M/floor(M/2),i)
+    imshow(mat2gray(reshape(bestEigenvectors(:,i),56,2576/56)));
+end
+
+
