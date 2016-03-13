5cf00c9624fc1db968f332431ab6ccf059b28131
Christoforos Santorinaios <Christoforos Santorinaios>
2016-03-13 22:42:06 +0000

    image files, code for Q4 that might be correct

--- Q4.m
+++ Q4.m
@@ -1,6 +1,6 @@
 %script for Q4 
 
-
+clearvars;
 load('face.mat') %load given data
 validationPC=0.1; %percentage of data to be used as validation data
 M=10; %the largest M eigenvalues will be picked. 
@@ -26,32 +26,35 @@ U=normc(U);
 u=U(:,1:M); %top M eigenvectors
 
 
+predictedLabels=[];
+confusionMatrix=zeros(size(testSet,2)); %initialise confusion matrix of size #testLabels x #testLabels
+for j=1:size(testSet,2)    %for each test image
+    testImage=testSet(:,j);  
+    phi=testImage-avgFace;  
+    omegaTest=phi'*u;   %find its omegas
 
-confusionMatrix=zeros(52,52);
-for j=1:size(testSet,2)
-    testImage=testSet(:,j);
-    phi=testImage-avgFace;
-    omegaTest=phi'*u;
-
-    norms=zeros(size(trainingSet,2),2);
-    for i=1:size(trainingSet,2)
-        norms(i,1)=norm(omegaTest-(A(:,i)'*u));
-        norms(i,2)=trainingLabels(i);
+    norms=zeros(size(trainingSet,2),2);  %initialise matrix of norms of size #trainingImages x 2
+    for i=1:size(trainingSet,2) % for each training image
+        norms(i,1)=norm(omegaTest-(A(:,i)'*u));  %calculate the L2 norm between omega and omegas of all training images
+        norms(i,2)=trainingLabels(i);  %attach the corresponding training data labels next to all these L2 norms
 
     end
-
-    [mins,I]=min(norms(:,1));
-    nearestNeighbour=trainingLabels(I);
-    confusionMatrix(i,j)=confusionMatrix(i,j)+(nearestNeighbour==testLabels(j));
+    [~,I]=min(norms(:,1)); %find index of minimum norm
+    NN=trainingLabels(I); %that minimum norm is the nearest neighbour
+    predictedLabels=[predictedLabels NN];
+    
+%     confusionMatrix(i,j)=confusionMatrix(i,j)+(nearestNeighbour==testLabels(:,j)); %record a success in the confusion matrix if the NN is same as the actual label for this data point
 
 end
+successes=(predictedLabels==testLabels);
+percentageAccuracy=sum(successes)/size(testLabels,2);
 
-figure;
-title('nearest neighbour image')
-showFace(trainingSet(:,I));
-figure;
-title('testImage');
-showFace(testImage);
+% figure;
+% title('nearest neighbour image')
+% showFace(trainingSet(:,I));  %show the face of the closest matching neighbour for index I
+% figure;
+% title('testImage');
+% showFace(testImage);
 
 
 
