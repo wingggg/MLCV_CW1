a637fc1d8556bd31428000ea399659c9eb73eaf4
Christoforos Santorinaios <Christoforos Santorinaios>
2016-03-15 23:29:57 +0000

    Q4 now prints its own results and figures, including confusion matrix

--- Q4.m
+++ Q4.m
@@ -1,9 +1,11 @@
 %script for Q4 
 
 clearvars;
+
 load('face.mat') %load given data
+tic
 validationPC=0.4; %percentage of data to be used as validation data
-M=250; %the largest M eigenvalues will be picked. 
+M=100; %the largest M eigenvalues will be picked. 
 
 [Train, Test] = crossvalind('HoldOut', size(X,2), validationPC); %produce crossvalidation indices for training and test sets
 trainingSet=X(:,Train); %create training set from X according to indices
@@ -27,6 +29,7 @@ u=U(:,1:M); %top M eigenvectors
 
 
 predictedLabels=[];
+nnImages=[];
 confusionMatrix=zeros(size(testSet,2)); %initialise confusion matrix of size #testLabels x #testLabels
 for j=1:size(testSet,2)    %for each test image
     testImage=testSet(:,j);  
@@ -37,24 +40,70 @@ for j=1:size(testSet,2)    %for each test image
     for i=1:size(trainingSet,2) % for each training image
         norms(i,1)=norm(omegaTest-(A(:,i)'*u));  %calculate the L2 norm between omega and omegas of all training images
         norms(i,2)=trainingLabels(i);  %attach the corresponding training data labels next to all these L2 norms
-
     end
     [~,I]=min(norms(:,1)); %find index of minimum norm
     NN=trainingLabels(I); %that minimum norm is the nearest neighbour
-    predictedLabels=[predictedLabels NN];
+    nnImages=[nnImages trainingSet(:,I)]; %store the NN image for this test image
+    predictedLabels=[predictedLabels NN]; %store predicted label
     
-%     confusionMatrix(i,j)=confusionMatrix(i,j)+(nearestNeighbour==testLabels(:,j)); %record a success in the confusion matrix if the NN is same as the actual label for this data point
 
 end
 successes=(predictedLabels==testLabels);
 percentageAccuracy=sum(successes)/size(testLabels,2);
-% 
-% figure;
-% title('nearest neighbour image')
-% showFace(trainingSet(:,I));  %show the face of the closest matching neighbour for index I
-% figure;
-% title('testImage');
-% showFace(testImage);
+CM=confusionmat(testLabels,predictedLabels);
+
+
+%WRITE RESULTS
+
+toc
+fileID = fopen('CW1_Q4Results.txt','a');
+fprintf(fileID,'Using NN classification\n');
+fprintf(fileID,'Test set is %d%% of train data\n',validationPC*100);
+fprintf(fileID,'M: %d \n',M);
+fprintf(fileID,'Accuracy: %f%% \n',percentageAccuracy*100);
+
+fmt=[repmat('%d ',1,52) '\n'];
+fprintf(fileID,'Confusion Matrix:\n');
+fprintf(fileID,fmt,CM');
+fprintf(fileID,'\n');
+fprintf(fileID,'Time duration: %f seconds\n',toc);
+fprintf(fileID,'===============================================\n');
+fclose(fileID);
+ 
+
+figure;
+
+title('Success Case');
+for i=1:length(testLabels)
+    if successes(i)==1
+        subplot(1,2,1);
+        showFace(testSet(:,i));
+        xlabel(['Class ',num2str(testLabels(i))]);
+        subplot(1,2,2);
+        showFace(nnImages(:,i));
+        xlabel(['Class  ',num2str(predictedLabels(i))]);
+        break;
+    end
+end
+successFig=gcf;
+figure;
+title('Failure Case')
+for i=1:length(testLabels)
+    if successes(i)==0
+        subplot(1,2,1);
+        showFace(testSet(:,i));
+        xlabel(['Class ',num2str(testLabels(i))]);
+        subplot(1,2,2);
+        showFace(nnImages(:,i));
+        xlabel(['Class  ',num2str(predictedLabels(i))]);
+        break;
+    end
+end
+failFig=gcf;
+
+%SAVE FIGURES
+saveas(successFig,'succFig.png');
+saveas(failFig,'failFig.png');
 
 
 
